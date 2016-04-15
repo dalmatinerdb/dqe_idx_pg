0c73dda53ca005d96de8b267f1862788f1b7d1d2
David Cromberge <david.cromberge@dataloop.io>
2016-04-15 17:56:36 +0200

    Fix the seed script

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@
 -- Data for Name: metrics; Type: TABLE DATA; Schema: public; Owner: davidcromberge
 --
 
-COPY metrics (id, tag_bucket, tag_metric, bucket, metric_0, metric_1, metric_2, metric_3, metric_4, metric_5, metric_6, metric_7, metric_8, metric_9) FROM stdin;
+COPY metrics (id, tag_bucket, bucket, tag_metric, metric_0, metric_1, metric_2, metric_3, metric_4, metric_5, metric_6, metric_7, metric_8, metric_9) FROM stdin;
 1	553e7645d2a9d06502f49e0b	55	one.two.three.four.five.six.seven.eight.nine	zero	one	two	three	four	five	six	seven	eight	nine
 2	553e7645d2a9d06502f49e0b	55	base.cpu	base	cpu	\N	\N	\N	\N	\N	\N	\N	\N
 3	553e7645d2a9d06502f49e0b	55	base.memory	base	memory	\N	\N	\N	\N	\N	\N	\N	\N
