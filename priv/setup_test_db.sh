2b9a78d289c9bf2ab5db660c79eabce825ad4865
davecromberge <davecromberge@gmail.com>
2016-10-06 18:07:05 +0100

    Metric variants (#25)
    * Upgrade to latest dqe_idx
    
    * Bootstrap test environment
    
    * Correction to README instructions
    
    * Code highlighting commands in README.md

---
+++ priv/setup_test_db.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -z $(which pg_ctl) ] ; then
+    echo "Please install Postgres 9.1 or above and add it to your PATH"
+    exit 1
+fi
+
+pg_ctl initdb -s -D datadir/
+pg_ctl -D datadir/ -o "-F -p 10433" start -w > /dev/null 2>&1
+createdb -p 10433 metric_metadata
+psql -d metric_metadata -p 10433 -f priv/schema.sql
---
+++ priv/stop_test_db.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ -z $(which pg_ctl) ] ; then
+    echo "Please install Postgres 9.1 or above and add it to your PATH"
+    exit 1
+fi
+
+pg_ctl stop -D datadir/
