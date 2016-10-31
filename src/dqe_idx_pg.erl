c55f3af2fb2892d418f04c96bda02f4de2ee945b
Patrick Robinson <patrick.robinson@envato.com>
2016-11-01 02:39:14 +1100

    Allow for setting IP or Hostname (#29)
    * Allow for setting IP or Hostname
    
    * Fixup pattern match
    
    * Remove default that would mask other config variables
    
    * Don't re-use variables

--- src/dqe_idx_pg.erl
+++ src/dqe_idx_pg.erl
@@ -22,7 +22,14 @@ init() ->
     Opts = [size, max_overflow, database, username, password],
     Opts1 = [{O, application:get_env(dqe_idx_pg, O, undefined)}
              || O <- Opts],
-    {ok, {Host, Port}} = application:get_env(dqe_idx_pg, server),
+    {Host, Port} = case application:get_env(dqe_idx_pg, server) of
+        {ok, {H, P}} ->
+            {H, P};
+        _ ->
+            {ok, H} = application:get_env(dqe_idx_pg, host),
+            {ok, P} = application:get_env(dqe_idx_pg, port),
+            {H, P}
+        end,
     pgapp:connect([{host, Host}, {port, Port} | Opts1]).
 
 lookup(Query) ->
